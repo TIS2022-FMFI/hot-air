1533271055039ad4821b855eefe34ee8f3501a6b
MarekDinka <dinka2@uniba.sk>
2023-01-05 03:44:47 +0100

    EXE fix

--- src/server/EXE/src/Burniee/Main.java
+++ src/server/EXE/src/Burniee/Main.java
@@ -4,9 +4,9 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length > 0) {
+        if (args.length == 2) {
             try {
-                Client client = new Client();
+                Client client = new Client(Integer.parseInt(args[1]));
                 if (args[0].charAt(0) == '$') {
                     client.performEndOfSegment(args[0].substring(1));
                 } else {
