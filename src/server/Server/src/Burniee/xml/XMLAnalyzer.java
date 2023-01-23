f2510d1730da4fffd78d5f52c765a288f64af018
IvanBohman <ivusko.bo@gmail.com>
2023-01-23 22:24:26 +0100

    Xml measurement feature added to analyzer

--- src/server/Server/src/Burniee/xml/XMLAnalyzer.java
+++ src/server/Server/src/Burniee/xml/XMLAnalyzer.java
@@ -64,7 +64,6 @@ public class XMLAnalyzer {
         }
     }
 
-    //attention, might return null
     private static AbstractMap.SimpleEntry<String, List<AbstractMap.SimpleEntry<String, String>>> getBlockTimeAndTemp(Element block){
         List<AbstractMap.SimpleEntry<String, String>> res = new ArrayList<>();
         String[] name = block.getAttributes().getNamedItem("NAME").getNodeValue().split("#");
@@ -91,8 +90,6 @@ public class XMLAnalyzer {
                     Double.parseDouble(s[1]);
                     res.add(new AbstractMap.SimpleEntry<>(s[0], s[1] + "$" + time));
                     visited.add(s[0]);
-//                    res.add(s[0]);
-//                    res.add(s[1] + "$" + time);
 
                 } catch (NumberFormatException e){
                     throw new NumberFormatException("Incorrect temperature value in block: "
@@ -102,12 +99,47 @@ public class XMLAnalyzer {
             for (String blower : blowers){
                 if (!visited.contains(blower)){
                     res.add(new AbstractMap.SimpleEntry<>(blower, "0$" + time));
-//                    res.add(blower);
-//                    res.add("0$" + time);
                 }
             }
 
             return new AbstractMap.SimpleEntry<>(name[0], res);
+        } catch (NullPointerException e){
+            return null;
+        }
+    }
+
+    private static AbstractMap.SimpleEntry<String, List<AbstractMap.SimpleEntry<String, String>>> getBlockTimeAndTemp(
+            Element block, List<String> blowers, List<String> temps){
+        List<AbstractMap.SimpleEntry<String, String>> res = new ArrayList<>();
+        String name = block.getAttributes().getNamedItem("NAME").getNodeValue();
+
+        try {
+            Element gen = getFirstElemChild(Objects.requireNonNull(getFirstElemChild(block)));
+            String type = Objects.requireNonNull(gen).getAttributes().getNamedItem("TYPE").getNodeValue();
+            String time = "";
+
+            if (Objects.equals(type, "PER")) {
+                double cyc = Double.parseDouble(gen.getAttributes().getNamedItem("CYC").getNodeValue());
+                double freq = Double.parseDouble(gen.getAttributes().getNamedItem("FREQ").getNodeValue());
+                time = String.valueOf(cyc / freq);
+            } else if (Objects.equals(type, "RAMP")) {
+                time = gen.getAttributes().getNamedItem("RTIME").getNodeValue();
+            } else if (Objects.equals(type, "WAVE")) {
+                time = waveforms.get(gen.getAttributes().getNamedItem("WAVE").getNodeValue());
+            }
+
+            for (int i = 0; i < blowers.size(); i++){
+                try{
+                    Double.parseDouble(temps.get(i));
+                    res.add(new AbstractMap.SimpleEntry<>(blowers.get(i), temps.get(i) + "$" + time));
+
+                } catch (NumberFormatException e){
+                    throw new NumberFormatException("Incorrect temperature value in block: "
+                            + block.getAttributes().getNamedItem("NAME"));
+                }
+            }
+
+            return new AbstractMap.SimpleEntry<>(name, res);
         } catch (NullPointerException e){
             return null;
         }
@@ -132,6 +164,26 @@ public class XMLAnalyzer {
         return res;
     }
 
+    private static List<AbstractMap.SimpleEntry<String, List<AbstractMap.SimpleEntry<String, String>>>> getThreadTimeAndTemp(
+            Element thread, List<String> blowers, List<String> temps){
+        List<AbstractMap.SimpleEntry<String, List<AbstractMap.SimpleEntry<String, String>>>> res = new ArrayList<>();
+        NodeList children = thread.getChildNodes();
+        for (int i = 0; i < children.getLength(); i++){
+            Node n = children.item(i);
+
+            if (n.getNodeType() == Node.ELEMENT_NODE && n.getNodeName().equals("B")){
+                res.add(getBlockTimeAndTemp((Element) n, blowers, temps));
+            } else if (n.getNodeType() == Node.ELEMENT_NODE && n.getNodeName().equals("CALL")){
+                res.addAll(subroutines.get(n.getAttributes().getNamedItem("BLK").getNodeValue()));
+            } else if (n.getNodeType() == Node.ELEMENT_NODE && n.getNodeName().equals("SEQ")){
+                List<AbstractMap.SimpleEntry<String, List<AbstractMap.SimpleEntry<String, String>>>> seq = getSequenceTimeAndTemp((Element) n, blowers, temps);
+                res.addAll(seq);
+            }
+        }
+
+        return res;
+    }
+
     private static List<AbstractMap.SimpleEntry<String, List<AbstractMap.SimpleEntry<String, String>>>> getSequenceTimeAndTemp(Element seq) {
         List<AbstractMap.SimpleEntry<String, List<AbstractMap.SimpleEntry<String, String>>>> result = new ArrayList<>();
         Element firstChild = getFirstElemChild(seq);
@@ -145,13 +197,40 @@ public class XMLAnalyzer {
         return result;
     }
 
+    private static List<AbstractMap.SimpleEntry<String, List<AbstractMap.SimpleEntry<String, String>>>> getSequenceTimeAndTemp(
+            Element seq, List<String> blowers, List<String> temps) {
+        List<AbstractMap.SimpleEntry<String, List<AbstractMap.SimpleEntry<String, String>>>> result = new ArrayList<>();
+        Element firstChild = getFirstElemChild(seq);
+        if (firstChild != null && firstChild.getNodeName().equals("THREAD")) {
+            List<AbstractMap.SimpleEntry<String, List<AbstractMap.SimpleEntry<String, String>>>> one_iteration = getThreadTimeAndTemp(firstChild, blowers, temps);
+            int repetitions = (int) Double.parseDouble(seq.getAttributes().getNamedItem("REP").getNodeValue());
+            for (int i = 0; i < repetitions; i++){
+                result.addAll(one_iteration);
+            }
+        }
+        return result;
+    }
+
     private static void setSubroutines(Document doc){
         NodeList subrts = doc.getElementsByTagName("SUBRT");
         for (int i = 0; i < subrts.getLength(); i++){
             Node subrt = subrts.item(i);
             Element firstChild = getFirstElemChild((Element) subrt);
+            String subrtName = ((Element) subrt).getAttribute("NAME");
             if (firstChild != null && firstChild.getNodeName().equals("THREAD")){
-                subroutines.put(subrt.getAttributes().getNamedItem("NAME").getNodeValue(), getThreadTimeAndTemp(firstChild));
+                if (subrtName.contains("Measurement") || subrtName.contains("measurement") || subrtName.contains("MEASUREMENT")){
+                    List<String> blowers = new ArrayList<>();
+                    List<String> temps = new ArrayList<>();
+                    String[] names = subrtName.split("#");
+                    for (int j = 1; j < names.length; j++){
+                        String[] name = names[j].split("@");
+                        blowers.add(name[0]);
+                        temps.add(name[1]);
+                    }
+                    subroutines.put(names[0], getThreadTimeAndTemp(firstChild, blowers, temps));
+                } else {
+                    subroutines.put(subrt.getAttributes().getNamedItem("NAME").getNodeValue(), getThreadTimeAndTemp(firstChild));
+                }
             }
         }
     }
