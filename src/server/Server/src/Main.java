7dc3e7f57cee9a7da41543973de4c0a55a98f742
MarekDinka <dinka2@uniba.sk>
2023-02-08 14:45:24 +0100

    quick quick quick quick quick quick quick quick quick quick quick quick quick quick fix

--- src/server/Server/src/Main.java
+++ src/server/Server/src/Main.java
@@ -1,7 +1,21 @@
 import Burniee.Server;
 
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        Server.getInstance().begin();
+        try {
+            if (args.length > 0 && args[0].equals("instance")) {
+                Server.getInstance().begin();
+            } else {
+                if (System.getProperty("os.name").startsWith("Linux")) {
+                    Runtime.getRuntime().exec("gnome-terminal -- java -jar Server.jar instance");
+                } else {
+                    Runtime.getRuntime().exec("cmd java -jar Server.jar instance");
+                }
+            }
+        } catch (IOException e) {
+            System.exit(-1);
+        }
     }
 }
